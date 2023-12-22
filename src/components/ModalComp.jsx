8ce59959eac426c0d3e94b60eb6088295dc2db34
marcod08 <you@example.com>
2023-12-22 16:15:54 +0100

    trasformo la navbar in un classcomponent

--- src/components/ModalComp.jsx
+++ src/components/ModalComp.jsx
@@ -46,7 +46,7 @@ class ModalComp extends Component {
       const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTg1OGM0OGI5ODkwODAwMTg0ODg3NGMiLCJpYXQiOjE3MDMyNTEwMTYsImV4cCI6MTcwNDQ2MDYxNn0.TB-26CMc7CFro3b2ohQR8fQE9FP9nAXd-5zof_thZdI";
 
       try {
-        // ho rimosso l'id dall'endpoint
+        // ho rimosso l'id
         const response = await fetch('https://striveschool-api.herokuapp.com/api/comments', {
           method: 'POST',
           headers: {
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+import React, { Component } from 'react';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
@@ -5,41 +6,59 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function MyNavbar() {
-    return (
+class MyNavbar extends Component {
+    render() {
+      return (
         <Navbar expand="lg" className="bg-dark">
-            <Container fluid>
-                <Navbar.Brand href="#"> <img src="/assets/images/logo.png" alt="netflix logo" width={70} /></Navbar.Brand>
-                <Navbar.Toggle aria-controls="navbarScroll" />
-                <Navbar.Collapse id="navbarScroll">
-                    <Nav
-                        className="me-auto my-2 my-lg-0"
-                        style={{ maxHeight: '100px' }}
-                        navbarScroll
-                    >
-                        <Nav.Link href="#" className='text-white fw-bold'>Home</Nav.Link>
-                        <Nav.Link href="#" className='text-white fw-bold'>Tv Shows</Nav.Link>
-                        <Nav.Link href="#" className='text-white fw-bold'>Movies</Nav.Link>
-                        <Nav.Link href="#" className='text-white fw-bold'>Recently Added</Nav.Link>
-                        <Nav.Link href="#" className='text-white fw-bold'>My List</Nav.Link>
-
-                    </Nav>
-                    <Form className="d-flex">
-                        <Form.Control
-                            type="search"
-                            placeholder="Search"
-                            className="me-2"
-                            aria-label="Search"
-                        />
-                        <Button variant='dark'><i className="bi bi-search text-white"></i></Button>
-                    </Form>
-                    <Nav.Link href="#" className='text-white fw-bold'>KIDS</Nav.Link>
-                    <Button variant='dark'><i className="bi bi-bell-fill text-white"></i></Button>
-                    <Button variant='dark'><i className="bi bi-person-fill text-white"></i></Button>
-                </Navbar.Collapse>
-            </Container>
+          <Container fluid>
+            <Navbar.Brand href="#">
+              {' '}
+              <img src="/assets/images/logo.png" alt="netflix logo" width={70} />
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbarScroll" />
+            <Navbar.Collapse id="navbarScroll">
+              <Nav className="me-auto my-2 my-lg-0" style={{ maxHeight: '100px' }} navbarScroll>
+                <Nav.Link href="#" className="text-white fw-bold">
+                  Home
+                </Nav.Link>
+                <Nav.Link href="#" className="text-white fw-bold">
+                  Tv Shows
+                </Nav.Link>
+                <Nav.Link href="#" className="text-white fw-bold">
+                  Movies
+                </Nav.Link>
+                <Nav.Link href="#" className="text-white fw-bold">
+                  Recently Added
+                </Nav.Link>
+                <Nav.Link href="#" className="text-white fw-bold">
+                  My List
+                </Nav.Link>
+              </Nav>
+              <Form className="d-flex">
+                <Form.Control
+                  type="search"
+                  placeholder="Search"
+                  className="me-2"
+                  aria-label="Search"
+                />
+                <Button variant="dark">
+                  <i className="bi bi-search text-white"></i>
+                </Button>
+              </Form>
+              <Nav.Link href="#" className="text-white fw-bold">
+                KIDS
+              </Nav.Link>
+              <Button variant="dark">
+                <i className="bi bi-bell-fill text-white"></i>
+              </Button>
+              <Button variant="dark">
+                <i className="bi bi-person-fill text-white"></i>
+              </Button>
+            </Navbar.Collapse>
+          </Container>
         </Navbar>
-    );
-}
-
-export default MyNavbar;
+      );
+    }
+  }
+  
+  export default MyNavbar;
